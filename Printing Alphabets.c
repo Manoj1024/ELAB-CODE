b1045c3eafc277f5ed58f90a6d03340e7aef96e1
Manoj1024 <61429322+Manoj1024@users.noreply.github.com>
2020-02-25 00:15:18 +0530

    Update Printing Alphabets.c

--- Printing Alphabets.c
+++ Printing Alphabets.c
@@ -1,17 +1,22 @@
     #include <stdio.h>
 #include<string.h>
-    int main() {
+    int main() 
+    {
         char line[150];
         int i,j;
         scanf("%149s", line);
-        for (i = 0; line[i] != '\0'; ++i) {
-            while (!((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || line[i] == '\0')) {
-                for (j = i; line[j] != '\0'; ++j) {
-                    line[j] = line[j + 1];
+        for (i = 0; line[i] != '\0'; ++i)
+       { 
+            while (!((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || line[i] == '\0')) 
+           {
+                for (j = i; line[j] != '\0'; ++j) 
+                {
+                  line[j] = line[j + 1];
                 }
+                
                 line[j] = '\0';
-            }
-        }
+           }
+       }
         
         puts(line);
         return 0;
