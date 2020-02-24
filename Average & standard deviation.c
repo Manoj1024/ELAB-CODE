a0c1cf3927b114e12209c2c8264cfc5975194f82
Manoj1024 <61429322+Manoj1024@users.noreply.github.com>
2020-02-25 00:20:49 +0530

    Update Average & standard deviation.c

--- Average & standard deviation.c
+++ Average & standard deviation.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <math.h>
-#define MAXSIZE 10
+#define MAX 10
 
 int main()
 {
-    float x[MAXSIZE];
+    float x[MAX];
     int  i, n;
     float sum=0,sum1=0,variance;
-int SD,Mean;
+    int SD,Mean;
     scanf("%d", &n);
  
     for (i = 0; i < n; i++)
-    {
+   {
         scanf("%f", &x[i]);
-    }
+   }
+    
     /*  Compute the sum of all elements */
-    for (i = 0; i < n; i++)
-    {
-        sum = sum + x[i];
-    }
-   Mean=sum/(float)n;
+      for (i = 0; i < n; i++)
+      {
+         sum = sum + x[i];
+      }
+    
+      Mean=sum/(float)n;
+    
     /*  Compute  variance  and standard deviation  */
-    for (i = 0; i < n; i++)
-    {
-        sum1 = sum1 + pow((x[i] -Mean), 2);
-    }
-   variance = sum1 / (float)n;
+       for (i = 0; i < n; i++)
+       {
+          sum1 = sum1 + pow((x[i] -Mean), 2);
+       }
+    variance = sum1 / (float)n;
     SD=sqrt(variance);
     printf("Mean=%d\n",Mean);
     printf("SD=%d\n",SD);
-return 0;
+    return 0;
 
 }
